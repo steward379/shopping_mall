277efa344d52f0bf7f20d1d607300a1bc340f9ef
Steward_tsou <steward379@gmail.com>
2017-08-23 12:02:32 +0800

    new

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,8 +4,40 @@ class Cart
     #     @items
     # end
 
-    def initialize
-        @items =[]
+    def initialize(items = [])
+        @items = items
+    end
+
+    def self.from_hash(hash)
+        if hash.nil? or hash["items"].nil?
+            # Cart.new
+            new
+        else
+            new hash["items"].map { |item|
+                CartItem.new(item["product_id"], item["quantity"])
+            }
+            # a = []
+            # a = hash["items"].each do |item|
+            #     a << CartItem.new(item["product_id"], item["quantity"])
+            # end
+            # Cart.new(a)
+        end
+
+    end
+
+    def serialize
+        a = []
+
+        # @items.each do |item|
+        #     a << { "product_id" => item.product_id,
+        #            "quantity" => item.quantity }
+        # end
+
+        all_items = items.map { |item|
+            { "product_id" => item.product_id, "quantity" => item.quantity }
+        }
+
+        { "items" => all_items }
     end
     
     def empty?
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -43,9 +43,33 @@ RSpec.describe Cart, type: :model do
   end
 
   describe "購物車進階功能" do
-    it "可以將購物車內容轉換成 Hash，存到 Session 裡"
-    it "可以把 Session 的內容（Hash 格式），還原成購物車的內容"
+
+    it "可以將購物車內容轉換成 Hash，存到 Session 裡" do 
+        cart = Cart.new
+        3.times { cart.add_item(2) }
+        4.times { cart.add_item(5) }
+        
+        expect(cart.serialize).to eq session_hash
+    end
+
+    it "可以把 Session 的內容（Hash 格式），還原成購物車的內容" do
+      cart =  Cart.from_hash(session_hash)
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.product_id).to be 2
+      expect(cart.items.first.quantity).to be 3
+      expect(cart.items.second.product_id).to be 5
+      expect(cart.items.second.quantity).to be 4
+    end
+  end
+
+  private
+  def session_hash
+    {
+      "items" => [
+        { "product_id" => 2, "quantity" => 3 },
+        { "product_id" => 5, "quantity" => 4 }
+      ]
+    }
   end
 end
-
 
